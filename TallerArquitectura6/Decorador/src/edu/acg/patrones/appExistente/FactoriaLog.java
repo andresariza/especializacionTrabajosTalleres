dafe82adf37ded09098bcc672c2464eeb3be8f0f
andresariza <andresgudlu@gmail.com>
2018-09-20 21:11:15 -0500

    Taller 6 arquitectura

---
+++ TallerArquitectura6/Decorador/src/edu/acg/patrones/appExistente/FactoriaLog.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.acg.patrones.appExistente;
+
+/**
+ *
+ * @author andres
+ */
+public class FactoriaLog {
+    private String VariableLog;
+    
+    public FactoriaLog(String VariableLog){
+        this.VariableLog = VariableLog;
+    }
+    
+}
